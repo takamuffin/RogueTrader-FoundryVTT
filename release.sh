af0ad20bd3400c27515d85d4e0f22ee47cb8984e
Alex Sansone <sansone.alexb@gmail.com>
2024-03-16 02:19:10 -0500

    improve release script

--- release.sh
+++ release.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 old_version=$(jq -r '.version' system.json)
+new_version=$(echo ${old_version} | awk -F. -v OFS=. '{$NF += 1 ; print}')
+echo "Old version: $old_version"
+echo "New version: $new_version"
 
-sed -i '' -e "s/$old_version/$1/g" system.json
-rm -rf v$1.zip
-zip -r v$1.zip .
-gh release create v$1 v$1.zip system.json -t "v$1" -n "v$1"
-rm -rf v$1.zip
+sed -i '' -e "s/$old_version/$new_version/g" system.json
+rm -rf v$new_version.zip
+zip -r v$new_version.zip .
+gh release create v$new_version v$new_version.zip system.json -t "v$new_version" -n "v$new_version"
+rm -rf v$new_version.zip
 
 git add .
-git commit -m "Release v$1"
+git commit -m "Release v$new_version"
+git push
