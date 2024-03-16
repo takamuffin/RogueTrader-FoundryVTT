3108d09bfac3f146941f3abe544cbc06a2b1e27f
Alex Sansone <sansone.alexb@gmail.com>
2024-03-16 01:12:10 -0500

    genericize release script

--- release.sh
+++ release.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-rm -rf *.zip
-zip -r v4.4.3.zip .
-gh release create v4.4.3 v4.4.3.zip system.json -t "v4.4.3" -n "v4.4.3"
+
+rm -rf $1.zip
+zip -r $1.zip .
+gh release create $1 $1.zip system.json -t "$1" -n "$1"
