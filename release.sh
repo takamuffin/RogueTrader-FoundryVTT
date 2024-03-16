12fda55cb212ddea1a35317d63be5280d58c0cae
Alex Sansone <sansone.alexb@gmail.com>
2024-03-16 02:14:16 -0500

    Release v4.4.10

--- release.sh
+++ release.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-rm -rf $1.zip
-zip -r $1.zip .
-gh release create $1 $1.zip system.json -t "$1" -n "$1"
-rm -rf $1.zip
+old_version=$(jq -r '.version' system.json)
+
+sed -i '' -e "s/$old_version/$1/g" system.json
+rm -rf v$1.zip
+zip -r v$1.zip .
+gh release create v$1 v$1.zip system.json -t "v$1" -n "v$1"
+rm -rf v$1.zip
+
+git add .
+git commit -m "Release v$1"
